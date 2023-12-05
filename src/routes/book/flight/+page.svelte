ece9bd035a0214a2c7c52f5a46d29cd86887f289
Everest <mdrc.pops@gmail.com>
2023-12-05 14:38:55 -0600

    testing if form errors don't display form

--- src/routes/book/flight/+page.svelte
+++ src/routes/book/flight/+page.svelte
@@ -16,3 +16,13 @@
         <button class="uk-button uk-button-primary"><span uk-search-icon></span>Search</button>
     </div>
 </form>
+
+<main>
+    <form>
+        <!-- Your form fields go here -->
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username">
+        <!-- Other form elements... -->
+        <button type="submit">Submit</button>
+    </form>
+</main>
