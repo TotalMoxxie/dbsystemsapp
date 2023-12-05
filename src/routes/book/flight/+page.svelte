a2de0659a0ce42b68ca57f68a800e98de35d2fa1
Everest <mdrc.pops@gmail.com>
2023-12-05 14:41:54 -0600

    trying to add a div instead this time

--- src/routes/book/flight/+page.svelte
+++ src/routes/book/flight/+page.svelte
@@ -1,3 +1,4 @@
+<div>
 <form class="uk-flex uk-flex-center uk-grid-collapse uk-margin-left uk-margin-right uk-margin-top" uk-grid>
     <div class="uk-width-5-6">
         <div class="uk-grid-collapse" uk-grid>
@@ -16,13 +17,4 @@
         <button class="uk-button uk-button-primary"><span uk-search-icon></span>Search</button>
     </div>
 </form>
-
-<main>
-    <form>
-        <!-- Your form fields go here -->
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username">
-        <!-- Other form elements... -->
-        <button type="submit">Submit</button>
-    </form>
-</main>
+</div>
