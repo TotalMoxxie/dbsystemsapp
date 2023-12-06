252b78e4864b0533dadc1ac2ecacd19037e95452
Everest <mdrc.pops@gmail.com>
2023-12-05 20:40:23 -0600

    test2

--- src/routes/book/flight/+page.svelte
+++ src/routes/book/flight/+page.svelte
@@ -24,8 +24,21 @@
 
         <legend class="uk-legend">Book a Flight</legend>
 
-        <div class="uk-margin">
-            <input class="uk-input" type="text" placeholder="Input" aria-label="Input">
+        <div class="uk-width-5-6">
+            <div class="uk-grid-collapse">
+                <div class="uk-width-1-3">
+                    <input class="uk-input" type="text" placeholder="MAF" aria-label="25">
+                </div>
+                <div class="uk-width-1-3">
+                    <input class="uk-input" type="text" placeholder="AUS" aria-label="25">
+                </div>
+                <div class="uk-width-1-3">
+                    <input class="uk-input" type="date">
+                </div>
+            </div>
+        </div>
+        <div >
+            <button class="uk-button uk-button-primary">Search</button>
         </div>
     </fieldset>
 </form>
