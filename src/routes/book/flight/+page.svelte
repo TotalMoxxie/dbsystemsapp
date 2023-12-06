f250e75b0781ed4f7e665bbb05ef5b65590b1c8d
Everest <mdrc.pops@gmail.com>
2023-12-05 20:55:09 -0600

    test4

--- src/routes/book/flight/+page.svelte
+++ src/routes/book/flight/+page.svelte
@@ -1,23 +1,23 @@
 <form>
     <fieldset class="uk-fieldset">
-
         <legend class="uk-legend">Book a Flight</legend>
-
-        <div class="uk-width-5-6">
-            <div class="uk-grid-collapse">
-                <div class="uk-width-1-3">
-                    <input class="uk-input" type="text" placeholder="MAF" aria-label="25">
-                </div>
-                <div class="uk-width-1-3">
-                    <input class="uk-input" type="text" placeholder="AUS" aria-label="25">
-                </div>
-                <div class="uk-width-1-3">
-                    <input class="uk-input" type="date">
+        <div class="uk-grid" uk-grid>
+            <div class="uk-width-5-6">
+                <div class="uk-grid-collapse" uk-grid>
+                    <div class="uk-width-1-3">
+                        <input class="uk-input" type="text" placeholder="MAF" aria-label="25">
+                    </div>
+                    <div class="uk-width-1-3">
+                        <input class="uk-input" type="text" placeholder="AUS" aria-label="25">
+                    </div>
+                    <div class="uk-width-1-3">
+                        <input class="uk-input" type="date">
+                    </div>
                 </div>
             </div>
-        </div>
-        <div >
-            <button class="uk-button uk-button-primary">Search</button>
+            <div >
+                <button class="uk-button uk-button-primary">Search</button>
+            </div>
         </div>
     </fieldset>
 </form>
