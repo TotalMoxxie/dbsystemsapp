0f6f3536aa03d62c6c7fb62b93cf19287aa8dd3f
Everest <mdrc.pops@gmail.com>
2023-12-06 17:40:44 -0600

    updated intro and tools page

--- src/routes/about/+layout.svelte
+++ src/routes/about/+layout.svelte
@@ -5,7 +5,7 @@
         <li class="uk-nav-divider"></li>
         <li><a href="/about/intro">Introduction</a></li>
         <li><a href="/about/tools">Tools</a></li>
-        <li><a href="/about/codesnip">Code Snippets</a></li>
+        <!--<li><a href="/about/codesnip">Code Snippets</a></li>-->
         <!-- Add more navbar items if needed -->
         </ul>
     </div>
--- src/routes/about/tools/+page.svelte
+++ src/routes/about/tools/+page.svelte
@@ -1,72 +1,61 @@
-    <div class="uk-text-left">
-        <div><h1>This is a tools Page</h1></div>
-        <div id="section1" class="uk-section">
-            <h2>Section 1</h2>
-            <!-- Content of section 1 -->
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-          </div>
-          
-          <div id="section2" class="uk-section">
-            <h2>Section 2</h2>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-          </div>
-          
-          <div id="section3" class="uk-section">
-            <h2>Section 3</h2>
-                <p>More Content</p>
-          </div>
-    </div>
+<div class="uk-text-left">
+  <div><h1>Tools</h1></div>
+  <div id="section1" class="uk-section">
+    <h3>Why We Switched From Amazon Amplify to Supabase:</h3>
 
+    <p>
+      Amazon Amplify provided a comprehensive set of tools for mobile and web applications but was complex and potentially excessive for our needs.
+    </p>
 
+    <p>
+      Supabase, an open-source alternative, offered essential services like database, authentication, and real-time capabilities in a simpler, more cost-effective package.
+    </p>
+
+    <p>
+      Our Reasoning: We encountered technical challenges with Amplify's complexity and cost. Supabase offered a streamlined and efficient solution better suited to the scope of our project.
+    </p>
+  </div>
+
+  <div id="section2" class="uk-section">
+    <h3>SvelteKit</h3>
+
+    <p>
+      React, while a robust library for user interfaces, presented us with
+      issues in larger bundle sizes and performance.
+    </p>
+
+    <p>
+      SvelteKit, building on Svelte's compiler-based approach, provides faster
+      performance and smaller bundles
+    </p>
+  </div>
+
+  <div id="section3" class="uk-section">
+    <h3>Our Move to Vercel for Deployment:</h3>
+
+    <p>
+      Vercel is known for its simplicity in deployment, particularly for JavaScript frameworks like SvelteKit.
+    </p>
+
+    <p>
+      Our Reasoning: Vercel enhanced our deployment process, offering a seamless transition from development to production. Its compatibility with SvelteKit was a key factor in our choice.
+    </p>
+  </div>
+  <div id="section4" class="uk-section">
+    <h3>Conclusion:</h3>
+
+    <p>
+      Addressing Technical Difficulties: Our initial technology stack presented challenges in scalability, performance, and complexity.
+    </p>
+
+    <p>
+      Improved File Management: The new stack allows for more efficient handling of files and deployments.
+    </p>
+    <p>
+      Enhanced Developer Experience: We now enjoy a more intuitive and faster development process.
+    </p>
+    <p>
+      This strategic shift in our technology stack is aligned with our project's evolving needs, promising better performance, simplicity in development, and cost efficiency.
+    </p>
+  </div>
+</div>
