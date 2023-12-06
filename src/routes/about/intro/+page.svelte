59f68fb11d0799d9a5efe1fd4062c80fa4c2f51c
Everest <mdrc.pops@gmail.com>
2023-12-06 17:17:15 -0600

    Edited Intro

--- src/routes/about/intro/+page.svelte
+++ src/routes/about/intro/+page.svelte
@@ -1,72 +1,132 @@
-<div>
-        <h1>This is an intro Page</h1>
-        <div id="section1" class="uk-section">
-            <h2>Section 1</h2>
-            <!-- Content of section 1 -->
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-          </div>
-          
-          <div id="section2" class="uk-section">
-            <h2>Section 2</h2>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-          </div>
-          
-          <div id="section3" class="uk-section">
-            <h2>Section 3</h2>
-                <p>More Content</p>
-          </div>
+<div class="uk-text-left">
+  <div><h1>This is a tools Page</h1></div>
+
+  <div id="section1" class="uk-section">
+    <h2>Section 1</h2>
+
+    <!-- Content of section 1 -->
+
+    <h3>1. Objectives:</h3>
+
+    <p>
+      Develop a comprehensive web/mobile application to provide users with a
+      seamless travel experience.
+    </p>
+
+    <p>
+      Enable users to track flights in real-time, plan their trips, and
+      potentially book flights.
+    </p>
+
+    <p>
+      Implement robust user authentication for secure access to personal data
+      and bookings.
+    </p>
+
+    <h3>2. Features and Functionality:</h3>
+
+    <h4>Flight Tracker:</h4>
+
+    <ul>
+      <li>Real-time flight tracking using data from airlines and airports.</li>
+
+      <li>
+        Display flight status, departure/arrival times, delays, gate
+        information, and more.
+      </li>
+
+      <li>
+        User-friendly interface for tracking multiple flights simultaneously.
+      </li>
+    </ul>
+
+    <h4>Itinerary/Trip Planner:</h4>
+
+    <ul>
+      <li>Create and manage travel itineraries.</li>
+
+      <li>
+        Add flights, accommodations, activities, and other details to the
+        itinerary.
+      </li>
+
+      <li>
+        Suggest popular attractions, restaurants, and transportation options at
+        the destination.
+      </li>
+    </ul>
+
+    <h4>Flight Booking (Optional):</h4>
+
+    <ul>
+      <li>Integration with airline APIs to search and book flights.</li>
+
+      <li>Display flight options, prices, and availability.</li>
+
+      <li>Secure payment processing for booking flights.</li>
+    </ul>
+
+    <h4>User Authentication:</h4>
+
+    <ul>
+      <li>Secure user registration and login.</li>
+
+      <li>
+        Personalized user profiles with saved itineraries and flight
+        preferences.
+      </li>
+
+      <li>User data protection and privacy measures.</li>
+    </ul>
+
+    <h3>3. Input and Output:</h3>
+
+    <p>
+      Input: User inputs flight details, travel preferences, and personal
+      information for booking (if implemented).
+    </p>
+
+    <p>
+      Output: Real-time flight information, travel itineraries, booking
+      confirmations, and user profiles.
+    </p>
+
+    <h3>4. Processes:</h3>
+
+    <ul>
+      <li>
+        Data collection: Collect flight data from reliable sources and integrate
+        with the app.
+      </li>
+
+      <li>
+        User interface design: Create an intuitive and user-friendly interface
+        for both web and mobile platforms.
+      </li>
+
+      <li>
+        Backend development: Implement the logic for flight tracking, itinerary
+        planning, and booking (if implemented).
+      </li>
+
+      <li>
+        User authentication: Develop secure authentication and user profile
+        management systems.
+      </li>
+    </ul>
+
+    <h3>5. Technology Stack:</h3>
+
+    <ul>
+      <li>Frontend: React (for web), React Native (for mobile)</li>
+
+      <li>Backend: Amazon RDS</li>
+
+      <li>User Authentication: Amazon Amplify</li>
+
+      <li>Real-time Data: WebSocket or Push Notifications</li>
+
+      <li>Hosting: AWS</li>
+    </ul>
+  </div>
 </div>
-
-
