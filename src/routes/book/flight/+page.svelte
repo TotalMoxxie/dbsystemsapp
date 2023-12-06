41df100e11c9b30bdac9db66d314753c2125349b
Everest <mdrc.pops@gmail.com>
2023-12-05 21:41:57 -0600

    built a card for flight offers. Need to refactor

--- src/routes/book/flight/+page.svelte
+++ src/routes/book/flight/+page.svelte
@@ -21,3 +21,46 @@
         </div>
     </fieldset>
 </form>
+
+<style>
+    /* Your component-specific styles here */
+    .gradient-bg {
+      background: linear-gradient(to right, orange, white);
+      /* Other card styles */
+      /* If you need to ensure the gradient spans the entire card */
+      /* You can set the display property to inline-block or block */
+      /* For example: */
+      display: block;
+    }
+  </style>
+
+<div class="uk-container">
+    <div class="uk-card uk-card-default uk-card-body uk-width-1-1 uk-margin-top gradient-bg">
+        <div class="uk-child-width-expand uk-grid-small uk-grid-match" uk-grid>
+            <div>
+                <div class="uk-card uk-card-default uk-card-body">
+                    <h3 class="uk-card-title">HH:MM - HH:MM</h3>
+                    <p>Airline</p>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-primary uk-card-body">
+                    <h3 class="uk-card-title">1 stop</h3>
+                    <p>AAA</p>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-secondary uk-card-body">
+                    <h3 class="uk-card-title">00:00</h3>
+                    <p>Duration</p>
+                </div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-secondary uk-card-body">
+                    <h3 class="uk-card-title">Starting at: $100</h3>
+                    <p>Class</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
